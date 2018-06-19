3359908dd5ebd6fc7a361a20e679ba6aa976de4f
Hacker <hacker@bainarm.cn>
2018-06-19 23:52:19 +0800

    arm64: dts: rockchip: add renjia board for rk3399 linux
    Change-Id: I06284292394e797d840f2c266a042069171e8ef2
    Signed-off-by: Hacker <hacker@bainarm.cn>

--- build_rk3399_evb.sh
+++ build_rk3399_evb.sh
@@ -5,4 +5,4 @@ make ARCH=arm64 rockchip_linux_defconfig
 #make ARCH=arm64  rk3399-sapphire-excavator-linux.img -j12
 #make ARCH=arm64  rk3399-firefly-linux.img -j12
 #make ARCH=arm64  rk3399-zhi01-818-linux.img -j12
-make ARCH=arm64 rk3399-binocular-818-linux.img -j12
+make ARCH=arm64 rk3399-renjia-808-linux.img -j12
